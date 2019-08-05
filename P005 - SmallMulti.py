36ba25fb7775f8c409bd2c284a2d014ec4e30de3
Khalid Abusamieh <37083798+kabusamieh@users.noreply.github.com>
2019-08-05 13:54:36 -0400

    Got proper answer
    P005 - SmallMulti.py

---
+++ P005 - SmallMulti.py
@@ -0,0 +1,51 @@
+##  @title      SmallMulti
+#
+#   @brief      Finds the smallest evenly divisible number by a range.
+#               Reference: https://www.python.org/ (open source)
+#               Reference: https://projecteuler.net/problem=5
+#
+#   @author     Khalid Abusamieh
+#   @date       08/05/2019
+#
+
+##  @fn     checkDiv()
+#
+#   @brief  Checks for a lack of remainder.
+#
+#   @param  n = Number
+#   @param  lst = List of numbers to divide by.
+#
+#   @return N/A
+#
+#   @author Khalid Abusamieh
+#
+#   @note   N/A
+#
+def checkDiv(n, lst):
+    return all(map(lambda y: n%y == 0, lst))
+
+##  @fn     main()
+#
+#   @brief  Main Function
+#
+#   @return N/A
+#
+#   @author Khalid Abusamieh
+#
+#   @note   N/A
+#
+def main():
+    foo = []
+    for h in range(1,21):
+        foo.append(h)
+
+    i = 0
+    while True:
+        i += 10
+
+        if checkDiv(i, foo) == True:
+            print(i)
+            break
+
+# Function call
+main()
